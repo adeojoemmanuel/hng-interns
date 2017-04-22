c3c08fa95f837bd47386c7ae7a1af5b1e815c546
Opia Chuks <chuks24ng@yahoo.co.uk>
2017-04-22 21:41:25 +0100

    editing script.js

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ const app = express();
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-
+const express = require('express');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -17,15 +24,15 @@ const forceSSL = function() {
     next();
   }
 }
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-
-
-app.use(forceSSL())
-// Heroku port
-
-console.log("we ARE HERE");
-app.listen(process.env.PORT || 8080);
