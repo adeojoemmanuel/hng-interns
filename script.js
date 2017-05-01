a0d87116dd0afde8648691ba295ae9d8d59975ee
Opia Chuks <chuks24ng@yahoo.co.uk>
2017-05-01 21:41:38 +0100

    editing package.json

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-// server.js
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
-
-
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
-const path = require('path');
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-
-
-app.use(forceSSL())
 // Heroku port
-
-console.log("we ARE HERE");
 app.listen(process.env.PORT || 8080);
